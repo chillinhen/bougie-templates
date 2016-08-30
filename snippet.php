7278d875b9e0788d0cb208cf46400082bb59cfaa
chillinhen <c.hillen@gmx.net>
2016-08-30 20:13:31 +0200

    feinschliff

--- snippet.php
+++ snippet.php
@@ -1,18 +1,38 @@
 
-<div class="panel col-4 news-<?= $this->archive->id ?> block<?= $this->class ?>"> 
-    <?php if($this->addImage === true && $this->singleSRC != null): ?>
-    <figure class="image_container<?php if($this->floatClass): ?> <?= $this->floatClass; ?><?php endif; ?>">
-        <img class="responsive-img" src="<?= $this->picture['img']['src'] ?>" alt="<?= $this->Headline ?>">
-    </figure>
-   <?php else : ?><h2><?= $this->linkHeadline ?></h2>
-			<?php endif; ?>
+    
+    
+    
+    <!DOCTYPE html>
+<html lang="<?= $this->language ?>"<?php if ($this->isRTL): ?> dir="rtl"<?php endif; ?>>
+    <head>
+        <meta charset="<?= $this->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 
-<?php if ($this->teaser): ?> 
-  <p class="teaser"><?php echo $this->teaser; ?></p> 
-<?php else: ?> 
-  <p class="teaser"><?php echo substr(strip_tags($this->text), 0, strpos(strip_tags($this->text), " ", 275)); ?>... <?php if ($this->text): ?> 
-<p class="more"><?php echo $this->more; ?></p> 
-<?php endif; ?></p> 
-<?php endif; ?>  
+        <title><?= $this->title ?></title>
+        <base href="<?= $this->base ?>">
 
-</div>
+        <meta name="robots" content="<?= $this->robots ?>">
+        <meta name="description" content="<?= $this->description ?>">
+        <meta name="keywords" content="<?= $this->keywords ?>">
+
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <!--        		<link rel="stylesheet" href="files/layout/fonts/MyFontsWebfontsKit.css">-->
+        <link rel="stylesheet" href="files/layout/css/jquery.fancybox.css">
+
+    <?= $this->viewport ?>
+    <?= $this->framework ?>
+    <?= $this->stylesheets ?>
+
+    <?= $this->head ?>
+
+
+<!--        <script src="files/layout/js/jquery.bxslider.min.js"></script>
+        <script src="files/layout/js/materialize.min.js"></script>
+        <script src="files/layout/js/jquery.fancybox.pack.js"></script>-->
+<!--        <script src="files/layout/js/queryloader2.min.js"></script>-->
+        <script src="files/layout/js/init.js"></script>
+        <!-- ToDo Delete Font Preloader -> Conflict with Bougie preloader? -->
+<!--        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+        <script src="files/layout/js/fonts.js"></script>-->
+<!--[if lt IE 9]><script src="<?= TL_ASSETS_URL ?>assets/html5shiv/<?= $GLOBALS['TL_ASSETS']['HTML5SHIV'] ?>/html5shiv-printshiv.min.js"></script><![endif]-->
+    </head>
